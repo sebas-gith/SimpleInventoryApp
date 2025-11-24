f26f4aa692e2305ea4ba16bf81c8a93b327c9da9
Sebastian Gonzalo Alvarez Concepcion <sebastianalvarezconcepcion@gmail.com>
2025-11-24 11:30:23 -0400

    Feature: Add ProductsController with GET and POST endpoints

--- InventoryAPI/Models/Product.cs
+++ InventoryAPI/Models/Product.cs
@@ -1,10 +1,33 @@
-namespace InventoryAPI.Models
+using Microsoft.AspNetCore.Mvc;
+using InventoryAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InventoryAPI.Controllers
 {
-    public class Product
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public int Stock { get; set; }
+        private static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 1200.50m, Stock = 10 },
+            new Product { Id = 2, Name = "Mouse", Price = 25.00m, Stock = 50 }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public ActionResult<Product> CreateProduct(Product newProduct)
+        {
+            newProduct.Id = products.Count > 0 ? products.Max(p => p.Id) + 1 : 1;
+            
+            products.Add(newProduct);
+            return CreatedAtAction(nameof(GetProducts), new { id = newProduct.Id }, newProduct);
+        }
     }
 }
